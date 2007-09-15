c50b4fa6816ee16b5149b934db1357a30bd3ad52
baueran <baueran@gmail.com>
2007-09-15 07:47:44 +0000

    - src/socket.cc: update licensing to reflect SSL use - README: update licensing to reflect SSL use - TODO: remove

--- src/socket.cc
+++ src/socket.cc
@@ -15,6 +15,9 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
 // USA.
+//
+// Note that this program is released under the GPL with the additional
+// exemption  that compiling, linking, and/or using OpenSSL is allowed.
 
 #include <iostream>
 #include <csignal>
